2edaddc7b31c92fe5ad03c8fc9b0d6a8452ff9b2
Luis Miguel Torres Villegas <lmtorresv@eafit.edu.co>
2024-03-02 23:18:54 -0500

    Use PostgreSQL database instead of SQLite

--- lancelot/settings.py
+++ lancelot/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = "lancelot.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "lancelot",
+        "USER": "lancelot",
+        "PASSWORD": "lancelot",
+        "HOST": "localhost",
     }
 }
 
